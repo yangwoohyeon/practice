d3b14539995eff4685a6cd92dd92632f2ae6d74f
yangwoohyeon <dngus0717@gmail.com>
2024-06-07 18:35:39 +0900

    자바

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,35 @@
 public class Animal {
+    void sound(){
+        System.out.println("Animal 크래스의 sound()");
+    }
+}
+class dog extends Animal{
+    void sound(){
+        System.out.println("왈왈");
+    }
+}
+
+class cat extends Animal{
+    void sound(){
+        System.out.println("야옹");
+    }
+}
+
+class DynamicCallTest{
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        dog d = new dog();
+        cat c = new cat();
+
+        Animal obj;
+
+        obj=animal;
+        obj.sound();
+
+        obj = d;
+        obj.sound();
+
+        obj = c;
+        obj.sound();
+    }
 }
---
+++ src/CarTest.java
@@ -0,0 +1,6 @@
+public class CarTest {
+    public static void main(String[] args) {
+        Car obj = new Car();
+        System.out.println("obj "+obj.getClass().getName());
+    }
+}
