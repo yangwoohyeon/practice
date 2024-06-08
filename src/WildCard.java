c99627faf61fb9e59946a3436e60c4c37ef9376a
yangwoohyeon <dngus0717@gmail.com>
2024-06-09 01:05:34 +0900

    자바

---
+++ src/WildCard.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class WildCard {
+    public static double sumOfList(List<? extends Number> list){
+        double s =0.0;
+        for(Number n : list){
+            s+=n.doubleValue();
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> li = Arrays.asList(1,2,3);
+        System.out.println(sumOfList(li));
+    }
+}
