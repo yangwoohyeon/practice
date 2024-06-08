1db6d7c8fa273d869504363f07fda9017ef8ec7e
yangwoohyeon <dngus0717@gmail.com>
2024-06-08 16:30:12 +0900

    자바

---
+++ src/ActionListenerTest4.java
@@ -0,0 +1,5 @@
+public class ActionListenerTest4 {
+    public static void main(String[] args) {
+        MyFrame4 t = new MyFrame4();
+    }
+}
--- src/MyFrame4.java
+++ src/MyFrame4.java
@@ -1,2 +1,31 @@
-public class MyFrame4 {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame4 extends JFrame { //무명클래스
+    private JButton button;
+    private JLabel label;
+
+    public MyFrame4(){
+        this.setSize(300,200);
+        this.setTitle("이벤트 예제");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        button = new JButton("버튼 눌러라");
+        label = new JLabel("아직 버튼이 안눌림");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==button){
+                    button.setText("버튼 눌림");
+                    label.setText("정상 작동");
+                }
+            }
+        });
+        panel.add(button);
+        panel.add(label);
+        this.add(panel);
+        this.setVisible(true);
+    }
 }
